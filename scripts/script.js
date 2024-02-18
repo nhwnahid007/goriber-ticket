dde56b55df4e059ffcbeb10a476703908cfe9b2f
nhwnahid007 <nhwnahid@gmail.com>
2024-02-18 23:44:06 +0600

    Dom manipulation with seat added and left seats

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,35 @@
 // alert("connect hoise")
+
+const btnElement = document.getElementsByClassName("all-btn");
+let count=0;
+let totalSeatLeft=40;
+
+for ( const btn of btnElement){
+    
+    btn.addEventListener('click',function(event){
+        // const seatName = event.target;
+        // console.log(seatName);
+        // seatName.classList.add('bg')
+
+        // event.target.classList.add('bg-freen')
+        
+        //added seats
+        count++;
+        const addedSeat = document.getElementById("added-seat");
+        addedSeat.innerText=count;
+
+
+
+        //totalLeftSeat 
+        totalSeatLeft--;
+        const seatLeftText = document.getElementById("seat-left");
+        seatLeftText.innerText= totalSeatLeft;
+
+
+
+
+        
+        
+        
+    })
+}
