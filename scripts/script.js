87a755d8d028ae3a5f103378385eae12fe2116ff
nhwnahid007 <nhwnahid@gmail.com>
2024-02-19 04:11:02 +0600

    Dom manipulation with append child and total cost

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@
 const btnElement = document.getElementsByClassName("all-btn");
 let count = 0;
 let totalSeatLeft = 40;
+let sum=0;
+const ticketPrice=550;
+
 
 for (const btn of btnElement) {
   btn.addEventListener("click", function (event) {
@@ -15,7 +18,7 @@ for (const btn of btnElement) {
     if (count <= 4) {
         
       const seatName = event.target;
-      console.log(seatName);    
+    //   console.log(seatName);    
       seatName.classList.add("bg-green");
       seatName.classList.add("pointer-events-none");
     //   seatName.setAttribute("disabled", true);
@@ -26,6 +29,37 @@ for (const btn of btnElement) {
       //totalLeftSeat
       const seatLeftText = document.getElementById("seat-left");
       seatLeftText.innerText = totalSeatLeft;
+
+
+      //appending
+      const appendContainer = document.getElementById("append-container");
+      const li=document.createElement('li')
+      const p1=document.createElement('p')
+      const p2=document.createElement('p')
+      const p3=document.createElement('p')
+        p1.innerText=event.target.innerText;
+        p2.innerText='Economy';
+        p3.innerText='550'
+      li.appendChild(p1)
+      li.appendChild(p2)
+      li.appendChild(p3)
+
+      appendContainer.appendChild(li)
+      console.log(appendContainer);
+
+        const totalPrice = document.getElementById("total-price").innerText;
+        let convertedTotalPrice=parseInt(totalPrice);
+        // sum =count *ticketPrice;
+        sum+=ticketPrice;
+
+        document.getElementById("total-price").innerText=sum;
+
+        
+
+        // console.log(sum);
+
+        
+
       
     } else {
       alert("No more than 4");
