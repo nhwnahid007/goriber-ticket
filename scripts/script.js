b5e53922527adda270a10ae2ec9d242f3259af48
nhwnahid007 <nhwnahid@gmail.com>
2024-02-19 13:10:26 +0600

    Dom manipulation with disabled apply  button

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,8 @@
 const btnElement = document.getElementsByClassName("all-btn");
 let count = 0;
 let totalSeatLeft = 40;
-let sum=0;
-const ticketPrice=550;
-
+let sum = 0;
+const ticketPrice = 550;
 
 for (const btn of btnElement) {
   btn.addEventListener("click", function (event) {
@@ -16,13 +15,11 @@ for (const btn of btnElement) {
 
     //added seats
     if (count <= 4) {
-        
       const seatName = event.target;
-    //   console.log(seatName);    
+      //   console.log(seatName);
       seatName.classList.add("bg-green");
       seatName.classList.add("pointer-events-none");
-    //   seatName.setAttribute("disabled", true);
-       
+      //   seatName.setAttribute("disabled", true);
 
       const addedSeat = document.getElementById("added-seat");
       addedSeat.innerText = count;
@@ -30,66 +27,80 @@ for (const btn of btnElement) {
       const seatLeftText = document.getElementById("seat-left");
       seatLeftText.innerText = totalSeatLeft;
 
-
       //appending
       const appendContainer = document.getElementById("append-container");
-      const li=document.createElement('li')
-      const p1=document.createElement('p')
-      const p2=document.createElement('p')
-      const p3=document.createElement('p')
-        p1.innerText=event.target.innerText;
-        p2.innerText='Economy';
-        p3.innerText='550'
-      li.appendChild(p1)
-      li.appendChild(p2)
-      li.appendChild(p3)
+      const li = document.createElement("li");
+      const p1 = document.createElement("p");
+      const p2 = document.createElement("p");
+      const p3 = document.createElement("p");
+      p1.innerText = event.target.innerText;
+      p2.innerText = "Economy";
+      p3.innerText = "550";
+      li.appendChild(p1);
+      li.appendChild(p2);
+      li.appendChild(p3);
 
-      appendContainer.appendChild(li)
-    //   console.log(appendContainer);
+      appendContainer.appendChild(li);
+      //   console.log(appendContainer);
 
-        const totalPrice = document.getElementById("total-price").innerText;
-        let convertedTotalPrice=parseInt(totalPrice);
-        // sum =count *ticketPrice;
-        sum+=ticketPrice;
+      const totalPrice = document.getElementById("total-price").innerText;
+      let convertedTotalPrice = parseInt(totalPrice);
+      // sum =count *ticketPrice;
+      sum += ticketPrice;
 
-        document.getElementById("total-price").innerText=sum;
+      document.getElementById("total-price").innerText = sum;
 
-        document.getElementById("grand-total").innerText = sum;
+      document.getElementById("grand-total").innerText = sum;
 
+      let grandTotal = 0;
 
-        let grandTotal=0;
+        if(count==4){
+            document.getElementById("apply").disabled = false;
+             document
+               .getElementById("apply")
+               .addEventListener("click", function () {
+                 const couponText = document.getElementById("coupon").value;
+                 console.log(couponText);
 
-        document.getElementById('apply').addEventListener('click',function(){
-            const couponText = document.getElementById("coupon").value;
-            console.log(couponText);
+                 if (couponText === "NEW15" && count >= 4) {
+                   grandTotal = sum * 0.85;
+                   document.getElementById("grand-total").innerText =
+                     grandTotal;
 
-            if(couponText === "NEW15" && count >=0){
-                grandTotal=sum*.85;
-                document.getElementById("grand-total").innerText=grandTotal;
-                //  const discountPriceText =
-                //    document.getElementById("discount-amount").innerText;
-                //    discountPriceText=grandTotal; 
-                
-            }
-            else {
-                console.log('pani kha');
-                alert('Not eligible')
-            }
-            
+                     document.getElementById("apply-coupon").classList.add('hidden');
+                   //  const discountPriceText =
+                   //    document.getElementById("discount-amount").innerText;
+                   //    discountPriceText=grandTotal;
+                 }
+                  else if (couponText === "Couple 20" && count >= 4) {
+                    grandTotal = sum * 0.8;
+                    document.getElementById("grand-total").innerText =
+                      grandTotal;
 
-        })
+                    document
+                      .getElementById("apply-coupon")
+                      .classList.add("hidden");
+                    //  const discountPriceText =
+                    //    document.getElementById("discount-amount").innerText;
+                    //    discountPriceText=grandTotal;
+                  } else {
+                    // console.log('pani kha');
+                    alert("Not eligible. You must have use valid coupon code");
+                  }
+               });
+        }
 
-
-        // console.log(sum);
-
-        
-
-      
+      // console.log(sum);
     } else {
-      alert("No more than 4");
+      alert("Maximum ticket limit crossed");
 
       return;
     }
   });
 }
 
+// if (count === 0){
+//     document.getElementById
+// }
+
+// document.getElementById("apply").disabled = true;
